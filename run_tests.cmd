a402a03790ee3709261cc6fdc240163e18ed4add
Michael Yeaney <michael.yeaney@gmail.com>
2013-01-30 16:59:39 -0500

    Updated path to nunit runner

--- run_tests.cmd
+++ run_tests.cmd
@@ -3,8 +3,7 @@
 pushd "%~dp0"
 
 rem Define locations for known sys calls
-set _nunit="c:\Program Files (x86)\NUnit 2.5.10\bin\net-2.0\nunit-console.exe"
-rem set _nunit="C:\Program Files (x86)\Nunit 2.6\bin\nunit-console.exe"
+set _nunit="C:\Program Files (x86)\Nunit 2.5.7\bin\net-2.0\nunit-console.exe"
 
 copy .\lib\nunit.framework.dll .\bin\nunit.framework.dll
 
